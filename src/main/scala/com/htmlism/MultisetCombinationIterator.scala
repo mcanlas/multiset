639b4bcaee5ba160be9b1d8de8f2f197e0b10574
Mark Canlas <github@htmlism.com>
2014-05-02 17:10:12 -0400

    added iterator stubs

---
+++ src/main/scala/com/htmlism/MultisetCombinationIterator.scala
@@ -0,0 +1,7 @@
+package com.htmlism
+
+class MultisetCombinationIterator[A](set: Multiset[A], choose: Int, accumulator: List[A] = Nil) extends Iterator[List[A]] {
+  def hasNext = ???
+
+  def next() = ???
+}
---
+++ src/test/scala/com/htmlism/MultisetCombinationIteratorSpec.scala
@@ -0,0 +1,6 @@
+package com.htmlism
+
+import org.specs2.mutable.Specification
+
+object MultisetCombinationIteratorSpec extends Specification {
+}
