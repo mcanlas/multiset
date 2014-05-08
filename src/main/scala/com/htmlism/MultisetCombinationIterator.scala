c3d4ad70ad702696806b809555aceeef3209bbee
Mark Canlas <github@htmlism.com>
2014-05-08 17:53:23 -0400

    promote accumulator and result to multisets

--- src/main/scala/com/htmlism/MultisetCombinationIterator.scala
+++ src/main/scala/com/htmlism/MultisetCombinationIterator.scala
@@ -1,6 +1,6 @@
 package com.htmlism
 
-class MultisetCombinationIterator[A](set: Multiset[A], choose: Int, accumulator: List[A] = Nil) extends Iterator[List[A]] {
+class MultisetCombinationIterator[A](set: Multiset[A], choose: Int, accumulator: Multiset[A] = Multiset.empty[A]) extends Iterator[Multiset[A]] {
   def hasNext = ???
 
   def next() = ???
