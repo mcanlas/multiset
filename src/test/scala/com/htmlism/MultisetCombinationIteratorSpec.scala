26b2766ea61f6685d82bbf80d62ae570d317a86a
Mark Canlas <github@htmlism.com>
2014-05-13 16:05:31 -0400

    removed type

--- src/test/scala/com/htmlism/MultisetCombinationIteratorSpec.scala
+++ src/test/scala/com/htmlism/MultisetCombinationIteratorSpec.scala
@@ -3,20 +3,21 @@ package com.htmlism
 import org.specs2.mutable.Specification
 
 object MultisetCombinationIteratorSpec extends Specification {
-  type TestIterator = MultisetCombinationIterator[Int]
-
   def multi(elements: Int*) = {
-    var set = Multiset(Map.empty[Int, Int])
+    var set = Multiset.empty[Int]
 
     for (e <- elements) set = set + e
 
     set
   }
 
+  val emptySet = multi()
+
   "An empty library" should {
     "yield no decks" in {
-      new TestIterator(Multiset.empty, 0).size === 0
-      new TestIterator(Multiset.empty, 1).size === 0
+      new MultisetCombinationIterator(emptySet, 0).size === 0
+      new MultisetCombinationIterator(emptySet, 1).size === 0
+      new MultisetCombinationIterator(emptySet, 3).size === 0
     }
   }
 }
