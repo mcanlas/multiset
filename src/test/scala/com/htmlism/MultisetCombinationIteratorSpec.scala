2d024a012f7735c808e651b320b65e60cb265243
Mark Canlas <github@htmlism.com>
2014-05-14 16:20:18 -0400

    corrected choose equals accumulator case

--- src/test/scala/com/htmlism/MultisetCombinationIteratorSpec.scala
+++ src/test/scala/com/htmlism/MultisetCombinationIteratorSpec.scala
@@ -16,10 +16,9 @@ object MultisetCombinationIteratorSpec extends Specification {
 
   "An empty library" should {
     "yield no decks" in {
-      new MultisetCombinationIterator(emptySet, 0).size === 0
       new MultisetCombinationIterator(emptySet, 1).size === 0
       new MultisetCombinationIterator(emptySet, 3).size === 0
-    }.pendingUntilFixed
+    }
   }
 
   "An iterator with chosen size smaller than the required size" should {
@@ -30,6 +29,7 @@ object MultisetCombinationIteratorSpec extends Specification {
 
   "An iterator with chosen size equal to the required size" should {
     "yield the required set" in {
+      new MultisetCombinationIterator(emptySet, 0).toList === emptySet :: Nil
       new MultisetCombinationIterator(emptySet, someSet.size, someSet).toList === someSet :: Nil
     }
   }
