b558f7e5f72a13940da64b9bad7ebc832e4c72f8
Mark Canlas <github@htmlism.com>
2014-10-06 23:10:06 -0400

    reduced scope of empty set

--- src/test/scala/com/htmlism/MultisetCombinationIteratorSpec.scala
+++ src/test/scala/com/htmlism/MultisetCombinationIteratorSpec.scala
@@ -11,26 +11,26 @@ class MultisetCombinationIteratorSpec extends Specification {
     set
   }
 
-  val emptySet = multi()
+  private val empty = Multiset.empty[Int]
   val someSet  = multi(5, 4, 7)
 
   "An empty library" should {
     "yield no decks" in {
-      new MultisetCombinationIterator(emptySet, 1).size === 0
-      new MultisetCombinationIterator(emptySet, 3).size === 0
+      new MultisetCombinationIterator(empty, 1).size === 0
+      new MultisetCombinationIterator(empty, 3).size === 0
     }
   }
 
   "An iterator with a chosen size less than the accumulator size" should {
     "yield no decks" in {
-      new MultisetCombinationIterator(emptySet, someSet.size - 1, someSet).size === 0
+      new MultisetCombinationIterator(empty, someSet.size - 1, someSet).size === 0
     }
   }
 
   "An iterator with chosen size equal to the required size" should {
     "yield the required set" in {
-      new MultisetCombinationIterator(emptySet, 0).toList === emptySet :: Nil
-      new MultisetCombinationIterator(emptySet, someSet.size, someSet).toList === someSet :: Nil
+      new MultisetCombinationIterator(empty, 0).toList === empty :: Nil
+      new MultisetCombinationIterator(empty, someSet.size, someSet).toList === someSet :: Nil
     }
   }
 
