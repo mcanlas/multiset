1f382b35c503f0c366d742a93fad90f8e307d23b
Mark Canlas <github@htmlism.com>
2014-11-04 07:50:25 -0500

    use scala style type parameter

--- src/main/scala/com/htmlism/multiset/Multiset.scala
+++ src/main/scala/com/htmlism/multiset/Multiset.scala
@@ -4,45 +4,45 @@ object Multiset {
   /**
    * Creates a multiset with the specified elements.
    */
-  def apply[T](elements: T*) = elements.foldLeft(Multiset.empty[T])((m, e) => m + e)
+  def apply[A](elements: A*) = elements.foldLeft(Multiset.empty[A])((m, e) => m + e)
 
   /**
    * Creates a multiset with the specified element counts.
    */
-  def withCounts[T](elements: (T, Int)*) = new Multiset(Map(elements: _*))
+  def withCounts[A](elements: (A, Int)*) = new Multiset(Map(elements: _*))
 
   /**
    * An empty multiset of type T.
    */
-  def empty[T]: Multiset[T] = new Multiset(Map.empty[T, Int])
+  def empty[A]: Multiset[A] = new Multiset(Map.empty[A, Int])
 }
 
-class Multiset[T](elementCounts: Map[T, Int])
-  extends Iterable[T]
-  with Function[T, Int] {
+class Multiset[A](elementCounts: Map[A, Int])
+  extends Iterable[A]
+  with Function[A, Int] {
   private val mappable = elementCounts.filter { case (k, count) => count > 0 }
 
   def iterator = elementCounts.iterator.flatMap { case (k, n)  =>
     List.fill(n)(k)
   }
 
-  def apply(element: T): Int = if (mappable.contains(element)) mappable(element) else 0
+  def apply(element: A): Int = if (mappable.contains(element)) mappable(element) else 0
 
-  def contains(element: T): Boolean = mappable.contains(element)
+  def contains(element: A): Boolean = mappable.contains(element)
 
-  def elements: Iterable[T] = mappable.keys
+  def elements: Iterable[A] = mappable.keys
 
-  def withMaximum(maximum: Int): Multiset[T] = new Multiset(mappable.map { case (k, n) => (k, if (n > maximum) maximum else n) })
+  def withMaximum(maximum: Int): Multiset[A] = new Multiset(mappable.map { case (k, n) => (k, if (n > maximum) maximum else n) })
 
-  def without(element: T): Multiset[T] = new Multiset(mappable - element)
+  def without(element: A): Multiset[A] = new Multiset(mappable - element)
 
-  def +(element: T): Multiset[T] = {
+  def +(element: A): Multiset[A] = {
     val count = if (mappable.contains(element)) mappable(element) + 1 else 1
 
     new Multiset(mappable + (element -> count))
   }
 
-  def ++(that: Multiset[T]): Multiset[T] = {
+  def ++(that: Multiset[A]): Multiset[A] = {
     val keys = (elements ++ that.elements).toList // relaxing the underlying uniqueness constraint for "performance"
 
     val pairs = for (k <- keys) yield {
@@ -52,12 +52,12 @@ class Multiset[T](elementCounts: Map[T, Int])
     new Multiset(pairs.toMap)
   }
 
-  def combinations(n: Int): MultisetCombinationIterator[T] = new MultisetCombinationIterator(this, n)
+  def combinations(n: Int): MultisetCombinationIterator[A] = new MultisetCombinationIterator(this, n)
 
   override def hashCode() = mappable.hashCode()
 
   override def equals(that: Any) = that match {
-    case set: Multiset[T] => this.mappable == set.mappable
+    case set: Multiset[A] => this.mappable == set.mappable
     case _ => false
   }
 }
