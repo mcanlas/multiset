7a5f278da03ae6ab35d74254a8ba7bd061d82c98
Mark Canlas <github@htmlism.com>
2014-11-06 22:59:58 -0500

    required is more clear

--- src/main/scala/com/htmlism/multiset/MultisetCombinationIterator.scala
+++ src/main/scala/com/htmlism/multiset/MultisetCombinationIterator.scala
@@ -1,14 +1,14 @@
 package com.htmlism.multiset
 
-class MultisetCombinationIterator[A](source: Multiset[A], choose: Int, accumulator: Multiset[A] = Multiset.empty[A]) extends Iterator[Multiset[A]] {
-  private var remainingSet = source withMaximum choose - accumulator.size
+class MultisetCombinationIterator[A](source: Multiset[A], choose: Int, requiredSet: Multiset[A] = Multiset.empty[A]) extends Iterator[Multiset[A]] {
+  private var remainingSet = source withMaximum choose - requiredSet.size
   private var currentElement: A = _
   private var count = 0
   private var subIterator = Option.empty[MultisetCombinationIterator[A]]
 
-  var hasNext = accumulator.size <= choose && remainingSet.size + accumulator.size >= choose
+  var hasNext = requiredSet.size <= choose && remainingSet.size + requiredSet.size >= choose
 
-  if (remainingSet.size + accumulator.size > choose && accumulator.size < choose)
+  if (remainingSet.size + requiredSet.size > choose && requiredSet.size < choose)
     shiftAndReload()
 
   def next(): Multiset[A] = if (hasNext) {
@@ -25,24 +25,24 @@ class MultisetCombinationIterator[A](source: Multiset[A], choose: Int, accumulat
             shiftAndReload()
           }
 
-          assert(count + remainingSet.size + accumulator.size >= choose)
+          assert(count + remainingSet.size + requiredSet.size >= choose)
 
           // subIterator must be re-evaluated because it may have been modified since the case match.
           // don't change `subIterator.get` to `iterator`!
           val next = subIterator.get.next()
-          hasNext = subIterator.get.hasNext || count + remainingSet.size + accumulator.size > choose
+          hasNext = subIterator.get.hasNext || count + remainingSet.size + requiredSet.size > choose
 
           next
         }
       case None =>
-        if (accumulator.size == choose) {
+        if (requiredSet.size == choose) {
           hasNext = false
 
-          accumulator
-        } else if (remainingSet.size + accumulator.size == choose) {
+          requiredSet
+        } else if (remainingSet.size + requiredSet.size == choose) {
           hasNext = false
 
-          accumulator ++ remainingSet
+          requiredSet ++ remainingSet
         } else {
           throw new RuntimeException("could not generate a new set for the base case")
         }
@@ -61,6 +61,6 @@ class MultisetCombinationIterator[A](source: Multiset[A], choose: Int, accumulat
   }
 
   private def reloadSubIterator() = {
-    subIterator = Some(new MultisetCombinationIterator(remainingSet, choose, accumulator ++ Multiset.withCounts(currentElement -> count)))
+    subIterator = Some(new MultisetCombinationIterator(remainingSet, choose, requiredSet ++ Multiset.withCounts(currentElement -> count)))
   }
 }
