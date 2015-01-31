a5180197950a6fe4fed57f63ee77da27cafdbb72
Mark Canlas <github@htmlism.com>
2015-01-31 18:05:13 -0500

    added count construction property

--- src/test/scala/com/htmlism/multiset/MultisetPropertySpec.scala
+++ src/test/scala/com/htmlism/multiset/MultisetPropertySpec.scala
@@ -8,4 +8,8 @@ class MultisetPropertySpec extends Properties("Multiset") {
   property("sequence construction") = forAll { (elements: Seq[Int]) =>
     Multiset(elements: _*).size == elements.size
   }
+
+  property("count construction") = forAll { counts: Seq[(String, Int)] =>
+    Multiset.withCounts(counts: _*).size == counts.toMap.values.filter(_ > 0).sum
+  }
 }
