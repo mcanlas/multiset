a3d618042ac55df6b990841318ab98b8508c8cbe
Mark Canlas <github@htmlism.com>
2015-02-03 18:23:07 -0500

    generalized element type

--- src/test/scala/com/htmlism/multiset/MultisetProperties.scala
+++ src/test/scala/com/htmlism/multiset/MultisetProperties.scala
@@ -3,42 +3,43 @@ package com.htmlism.multiset
 import org.scalacheck.Properties
 
 class MultisetProperties extends Properties("Multiset") {
+  type A = Char
 
   import org.scalacheck.Prop.{ forAll, BooleanOperators }
 
-  property("sequence construction") = forAll { (elements: Seq[Int]) =>
+  property("sequence construction") = forAll { (elements: Seq[A]) =>
     Multiset(elements: _*).size == elements.size
   }
 
-  property("count construction") = forAll { counts: Seq[(String, Int)] =>
+  property("count construction") = forAll { counts: Seq[(A, Int)] =>
     Multiset.withCounts(counts: _*).size == counts.toMap.values.filter(_ > 0).sum
   }
 
-  property("iteration") = forAll { (elements: Seq[Int]) =>
+  property("iteration") = forAll { (elements: Seq[A]) =>
     Multiset(elements: _*).iterator.toList.sorted == elements.sorted
   }
 
-  property("containment") = forAll { (value: Char) =>
+  property("containment") = forAll { (value: A) =>
     Multiset()(value) == 0 && Multiset(value)(value) == 1
   }
 
-  property("uniqueness") = forAll { (elements: Seq[Int]) =>
+  property("uniqueness") = forAll { (elements: Seq[A]) =>
     Multiset(elements: _*).elements == elements.toSet
   }
 
-  property("maximum") = forAll { (counts: Seq[(String, Int)], maximum: Int) =>
+  property("maximum") = forAll { (counts: Seq[(A, Int)], maximum: Int) =>
     (maximum >= 0 && maximum < (Int.MaxValue / 100)) ==>
       (Multiset.withCounts(counts: _*).withMaximum(maximum).size <= counts.toMap.count(c => c._2 > 0) * maximum)
   }
 
-  property("without") = forAll { (element: String, count: Int) =>
+  property("without") = forAll { (element: A, count: Int) =>
     val emptyWithout = Multiset().without(element) == Multiset()
     val withWithout  = Multiset.withCounts(element -> count).without(element) == Multiset()
 
     emptyWithout && withWithout
   }
 
-  property("union") = forAll { (firstElement: String, secondElement: String, count: Int) =>
+  property("union") = forAll { (firstElement: A, secondElement: A, count: Int) =>
     val firstSet  = Multiset.withCounts(firstElement -> count)
     val secondSet = Multiset.withCounts(firstElement -> count, secondElement -> count)
 
