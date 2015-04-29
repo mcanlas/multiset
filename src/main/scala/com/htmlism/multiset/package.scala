be909bc74ac1dc209a63d734fc39c7b003d83a88
Mark Canlas <github@htmlism.com>
2015-04-29 15:50:12 -0400

    revised introduction

--- src/main/scala/com/htmlism/multiset/package.scala
+++ src/main/scala/com/htmlism/multiset/package.scala
@@ -1,8 +1,8 @@
 package com.htmlism
 
 /**
- * The `Multiset` trait extends Scala's collections library with an unordered collection that supports duplicate
- * elements. Contrast with `Set`, which only supports unique elements.
+ * A `Multiset` is an unordered collection that supports duplicate elements. Contrast with Scala's `Set`, which only
+ * supports unique elements.
  *
  * The trait is backed by two concrete implementations, `MapMultiset` and `SeqMultiset`, with the former being the
  * default. Ultimately, `Multiset` is a new subtype of `Iterable`, akin to `Seq`, `Set`, and `Map`.
@@ -13,6 +13,14 @@ package com.htmlism
  *   val cities     = Multiset('NewYork, 'NewYork, 'London)
  *   val characters = MapMultiset('Chrono, 'Marle, 'Lucca, 'Chrono)
  *   val elements   = SeqMultiset('Earth, 'Metal, 'Wood, 'Metal)
+ * }}}
+ *
+ * `Multiset` extends Scala's `Iterable` and thus shares in a wealth of operations from the collections library.
+ *
+ * {{{
+ *   cities.toList
+ *   cities.filter(_ == 'London)
+ *   cities.map(_ => 'Tokyo)
  * }}}
  */
 
